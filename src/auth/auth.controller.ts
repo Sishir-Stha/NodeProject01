956bfb299207df6089ed8a4c8159c165dcfe02e2
Sishir Shrestha <sishershrestha000@gmail.com>
2026-03-29 00:18:23 +0545

    controller and service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,20 +9,25 @@ export const login = async (req: Request, res: Response) => {
         const { email, password } = req.body;
         const user = await userService.login(email, password);
         if(!user){
-            return errorResponse(HttpStatus)
+            return errorResponse(HttpStatus.UNAUTHORIZED )
         }
         return successResponse(HttpStatus.OK)(
             res,
             'Login successful'  
         )(user);
     } catch (error) {
-        res.status(500).json({ message: "Internal server error" });
+        res.status(HttpStatus.BAD_REQUEST).json({ message: "Internal server error" });
     }
 }
 
 export const allUsers = async (req: Request, res: Response) => {
     try {
         const result = await userService.getAllUsers();
-        return successResponse
+        return successResponse(HttpStatus.OK)(
+            res,
+            'User Data Fetched'
+        )(result);
+    }catch (error) {
+        res.status(500).json({ message: "Internal server error" });
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,11 +12,27 @@ export const getAllUsers = async () => {
 export const login = async (email : string, password : string) => {
     //make the query to get the user with the given email and password from the database
     const query = 'Select * from users where email = $1 and password = $2';
-    // the values to be used in the query
+    /* * the values to be used in the query
+       * the values sending is purely position do email : $1 and password is $2 if i change there
+         position then email = password and passsword = email so remeber that 
+     */
     const values = [email, password];
     //send the query and the values to the database and return the result
     const result = await pool.query(query, values);
-    //return the result as a user object
+    /*
+        *  here the result format will look like this 
+                {
+            rows: [ {...}, {...}, ... ],
+            rowCount: 1,
+            command: 'SELECT',
+                }
+
+        * but we just need rows so if we just want rows then just send 
+            return result.row[]
+        *  this return the array of row but if we do the [0] then it will only sending the first 
+            row that is good for login since it will only have one row
+        *  return the result as a user object
+    */
     return result.rows[0];
 }
 
